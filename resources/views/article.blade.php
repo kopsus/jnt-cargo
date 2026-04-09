ba4d5f7619b93250cdd8a4fdb81bbbcd21eb840a
kopsus <emonsetio01@gmail.com>
2026-04-09 21:47:02 +0700

    feat : create article page

---
+++ resources/views/article.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    {{-- Semua kode di dalam tag ini akan otomatis masuk ke bagian {{ $slot }} di layout utama --}}
+    <section class="bg-primary py-12 md:py-20 px-6">
+        <div class="w-full max-w-7xl mx-auto space-y-8 md:space-y-16">
+            <p class="text-3xl md:text-5xl text-center font-bold text-white">Artikel</p>
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+                <div class="bg-white shadow rounded-xl p-4 space-y-2">
+                    <div class="w-full h-52 bg-primary rounded-xl"></div>
+                    <p class="text-xl font-medium">Activities</p>
+                    <p class="text-2xl md:text-3xl font-bold">Jasa Kirim Motor Termurah di Jogja</p>
+                    <p>Dengan dukungan jaringan nasional, harga kompetitif, layanan pickup area Jogja, serta sistem
+                        tracking yang transparan, pengiriman motor menjadi lebih mudah dan aman.</p>
+                    <p class="text-sm text-gray-600">february 23, 2026</p>
+                </div>
+                <div class="bg-white shadow rounded-xl p-4 space-y-2">
+                    <div class="w-full h-52 bg-primary rounded-xl"></div>
+                    <p class="text-xl font-medium">Activities</p>
+                    <p class="text-2xl md:text-3xl font-bold">Jasa Kirim Motor Termurah di Jogja</p>
+                    <p>Dengan dukungan jaringan nasional, harga kompetitif, layanan pickup area Jogja, serta sistem
+                        tracking yang transparan, pengiriman motor menjadi lebih mudah dan aman.</p>
+                    <p class="text-sm text-gray-600">february 23, 2026</p>
+                </div>
+                <div class="bg-white shadow rounded-xl p-4 space-y-2">
+                    <div class="w-full h-52 bg-primary rounded-xl"></div>
+                    <p class="text-xl font-medium">Activities</p>
+                    <p class="text-2xl md:text-3xl font-bold">Jasa Kirim Motor Termurah di Jogja</p>
+                    <p>Dengan dukungan jaringan nasional, harga kompetitif, layanan pickup area Jogja, serta sistem
+                        tracking yang transparan, pengiriman motor menjadi lebih mudah dan aman.</p>
+                    <p class="text-sm text-gray-600">february 23, 2026</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layout>
